d6a0acfdd94656acaf8790efa3f32543c26be962
Ayden Khalil <aydenkhalil619@gmail.com>
2026-04-23 23:55:11 +1000

    fixed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Template App',
-  description: 'A blank starter template',
+  title: 'Property Scores',
+  description: 'A property logs and scores app',
 };
 
 export default function RootLayout({
